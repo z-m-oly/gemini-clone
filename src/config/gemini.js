cf6abed1ee7fdb0f279ebdbe14310281e0552a3a
Zakaria Maalim <olyblv@gmail.com>
2025-04-11 21:35:12 +0300

    Update gemini.js
    ApiKey problems solved

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,38 +1,33 @@
-
-
-
-
 import {
-    GoogleGenerativeAI,
-    HarmCategory,
-    HarmBlockThreshold,
-  } from"@google/generative-ai";
-  
-  const apiKey = "AIzaSyDaW3y3_KNuCC6IscPPEznhGk8VVujSvLE";
-  const genAI = new GoogleGenerativeAI(apiKey);
-  
-  const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [],
   });
-  
-  const generationConfig = {
-    temperature: 1,
-    topP: 0.95,
-    topK: 40,
-    maxOutputTokens: 8192,
-    responseMimeType: "text/plain",
-  };
-  
-  async function run(prompt) {
-    const chatSession = model.startChat({
-      generationConfig,
-      history: [
-      ],
-    });
-  
-    const result = await chatSession.sendMessage(prompt);
-    console.log(result.response.text());
-    return result.response.text()
-  }
-  
-export default  run;
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+  return result.response.text();
+}
+
+export default run;
